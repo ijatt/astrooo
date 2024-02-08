46302e71ed35b8307b0ff09381ad5e26381f8483
ijattt <mshahrizat05@gmail.com>
2024-02-08 10:41:58 +0800

    Added prisma and supabase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,11 +4,17 @@ export default defineNuxtConfig({
   pages: true,
   runtimeConfig: {
     MONGO_URI: process.env.MONGO_URI,
-    SECRET_KEY: process.env.SECRET_KEY
+    SECRET_KEY: process.env.SECRET_KEY,
+    public: {
+      SUPABASE_URL: process.env.SUPABASE_URL,
+      SUPABASE_KEY: process.env.SUPABASE_KEY,
+      BUCKET_URL: process.env.BUCKET_URL,
+    }
   },
   modules: [
     '@pinia/nuxt', 
     'nuxt-icon',
-    '@nuxt/ui'
+    '@nuxt/ui',
+    '@pinia-plugin-persistedstate/nuxt',
   ]
 })
