0aa2a5789e9b9d2312f994ff2128ee5bef3c8f0c
ijattt <mshahrizat05@gmail.com>
2024-02-13 10:40:30 +0800

    Revert "added loading animation"
    This reverts commit f8ae0292e6a1a18455f26250f92e63f57f105447.

--- pages/index.vue
+++ pages/index.vue
@@ -2,26 +2,10 @@
   <div class="max-w-xl p-4 mx-auto">
     <CreatePost @refresh="refresh" />
     <ThePost v-for="post in posts" :key="post.id" :post="post" />
-    <div v-if="posts.length === 0">
-      <ClientOnly>
-        <Vue3Lottie 
-          :animation-data="astro"
-          :speed="1"
-          :loop="true"
-          :autoplay="true"
-          style="width: 300px; height: 300px;"
-        />
-        <p class="text-slate-600 text-center italic">
-          Loading...
-        </p>
-      </ClientOnly>
-    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { Vue3Lottie } from "vue3-lottie";
-import astro from "~/public/astro.json"
 import type { UserData } from "~/types/user";
 import type { Post } from "~/types/post";
 definePageMeta({
