bb385cb16c1715c76895975462cd5a27a9986e25
ijattt <mshahrizat05@gmail.com>
2024-02-13 09:39:07 +0800

    update supabase.ts

--- server/db/prisma.ts
+++ server/db/prisma.ts
@@ -1,6 +1,21 @@
 import Prisma from "@prisma/client";
-
 const { PrismaClient } = Prisma;
-const prisma = new PrismaClient();
+
+declare global {
+    // @ts-ignore
+    var prisma: PrismaClient | undefined;
+}
+
+// @ts-ignore
+let prisma: PrismaClient | null = null;
+
+if (process.env.NODE_ENV === "production") {
+    prisma = new PrismaClient();
+} else {
+    if (!global.prisma) {
+        global.prisma = new PrismaClient();
+    }
+    prisma = global.prisma;
+}
 
 export default prisma;
