c90a367c1a3a316ca7214fc91e94ddca54e31ba1
ijattt <mshahrizat05@gmail.com>
2024-02-15 14:38:23 +0800

    fix:(error while nagivating the page for the first time)

--- pages/[username]/post/[id].vue
+++ pages/[username]/post/[id].vue
@@ -1,64 +1,80 @@
 <template>
   <div class="max-w-xl p-4 mx-auto">
-    <div class="flex gap-2 items-center">
-      <img class="w-10 h-10 rounded-full object-cover" 
-        :src="post.users.image_url ? `${$config.public.BUCKET_URL}/avatars/${post.users.image_url}` : '/profile.png' " 
-      />
-      <div class="flex flex-col cursor-pointer" @click="navigateTo(`/${post.users.username}`)">
-        <h1 class="font-semibold hover:underline tracking-wide text-slate-600 dark:text-slate-300">{{ post.users.first_name }} {{ post.users.last_name }}</h1>
-            <p class="text-sm font-semibold tracking-wide text-slate-500 dark:text-slate-400">
-              @{{ post.users.username }}
-            </p>
+    <div v-if="post">
+      <div class="flex gap-2 items-center">
+        <img class="w-10 h-10 rounded-full object-cover" 
+          :src="post.users.image_url ? `${$config.public.BUCKET_URL}/avatars/${post.users.image_url}` : '/profile.png' " 
+        />
+        <div class="flex flex-col cursor-pointer" @click="navigateTo(`/${post.users.username}`)">
+          <h1 class="font-semibold hover:underline tracking-wide text-slate-600 dark:text-slate-300">{{ post.users.first_name }} {{ post.users.last_name }}</h1>
+              <p class="text-sm font-semibold tracking-wide text-slate-500 dark:text-slate-400">
+                @{{ post.users.username }}
+              </p>
+        </div>
+      </div>
+      <div class="mt-4 space-y-2">
+        <p class="text-slate-600 dark:text-slate-300">
+          {{ post.content }}
+        </p>
+        <div class="grid grid-flow-col gap-2">
+          <template v-if="post.images?.length" v-for="img in post.images">
+            <img :src="`${config.public.BUCKET_URL}/posts/${img.image_url}`" class="hover:shadow-md object-cover m-auto rounded-md border-slate-200" alt="" />
+          </template>
+        </div>
+      </div>
+      <p class="mt-4 text-sm font-semibold tracking-wide text-slate-500 dark:text-slate-400">{{ formattedDate }}</p>
+      <div class="mt-2 pb-2 flex justify-between border-b border-slate-200 dark:border-slate-700">
+        <div class="flex items-center group cursor-pointer">
+          <button @click="likePost" v-if="isLiked" class="flex items-center text-slate-600 font-semibold p-1.5 rounded-full group-hover:bg-indigo-200 group-hover:text-indigo-600">
+            <Icon name="mdi:heart" class="w-5 h-5 text-indigo-600" />
+          </button>
+          <button @click="likePost" v-else class="flex items-center text-slate-600 font-semibold p-1.5 rounded-full group-hover:bg-indigo-200 group-hover:text-indigo-600">
+            <Icon name="mdi:heart-outline" class="w-5 h-5" />
+          </button>
+          <p
+            class="text-slate-600 dark:text-slate-400 font-semibold tracking-wide group-hover:text-indigo-600"
+          >
+            {{ likeCount }}
+          </p>
+        </div>
+        <div class="flex items-center group cursor-pointer">
+          <button @click="open = true" class="flex items-center text-slate-600 dark:text-slate-400 font-semibold p-1.5 rounded-full group-hover:bg-indigo-200 group-hover:text-indigo-600">
+            <Icon name="mdi:comment-outline" class="w-5 h-5" />
+          </button>
+          <p
+            class="text-slate-600 dark:text-slate-400 font-semibold tracking-wide group-hover:text-indigo-600"
+          >
+            {{ commentCount }}
+          </p>
+        </div>
+        <div class="flex items-center group cursor-pointer">
+          <button class="flex items-center text-slate-600 dark:text-slate-400 font-semibold p-1.5 rounded-full group-hover:bg-indigo-200 group-hover:text-indigo-600">
+            <Icon name="mdi:share-outline" class="w-5 h-5" />
+          </button>
+          <p
+            class="text-slate-600 dark:text-slate-400 font-semibold tracking-wide group-hover:text-indigo-600"
+          >
+            102
+          </p>
+        </div>
+      </div>
+      <div class="">
+        <TheComment @refresh="refresh()" v-for="comment in post.comments" :key="comment.id" :comment="comment" />
       </div>
     </div>
-    <div class="mt-4 space-y-2">
-      <p class="text-slate-600 dark:text-slate-300">
-        {{ post.content }}
-      </p>
-      <div class="grid grid-flow-col gap-2">
-        <template v-if="post.images?.length" v-for="img in post.images">
-          <img :src="`${config.public.BUCKET_URL}/posts/${img.image_url}`" class="hover:shadow-md object-cover m-auto rounded-md border-slate-200" alt="" />
-        </template>
-      </div>
-    </div>
-    <p class="mt-4 text-sm font-semibold tracking-wide text-slate-500 dark:text-slate-400">{{ formattedDate }}</p>
-    <div class="mt-2 pb-2 flex justify-between border-b border-slate-200 dark:border-slate-700">
-      <div class="flex items-center group cursor-pointer">
-        <button @click="likePost" v-if="isLiked" class="flex items-center text-slate-600 font-semibold p-1.5 rounded-full group-hover:bg-indigo-200 group-hover:text-indigo-600">
-          <Icon name="mdi:heart" class="w-5 h-5 text-indigo-600" />
-        </button>
-        <button @click="likePost" v-else class="flex items-center text-slate-600 font-semibold p-1.5 rounded-full group-hover:bg-indigo-200 group-hover:text-indigo-600">
-          <Icon name="mdi:heart-outline" class="w-5 h-5" />
-        </button>
-        <p
-          class="text-slate-600 dark:text-slate-400 font-semibold tracking-wide group-hover:text-indigo-600"
-        >
-          {{ likeCount }}
+    <div v-if="!post">
+      <ClientOnly>
+        <Vue3Lottie 
+          :animation-data="astro"
+          :speed="1"  
+          :loop="true"
+          :autoplay="true"
+          style="width: 300px; height: 300px;"
+        />
+        <p class="text-slate-600 text-center italic">
+          Loading...
         </p>
-      </div>
-      <div class="flex items-center group cursor-pointer">
-        <button @click="open = true" class="flex items-center text-slate-600 dark:text-slate-400 font-semibold p-1.5 rounded-full group-hover:bg-indigo-200 group-hover:text-indigo-600">
-          <Icon name="mdi:comment-outline" class="w-5 h-5" />
-        </button>
-        <p
-          class="text-slate-600 dark:text-slate-400 font-semibold tracking-wide group-hover:text-indigo-600"
-        >
-          {{ commentCount }}
-        </p>
-      </div>
-      <div class="flex items-center group cursor-pointer">
-        <button class="flex items-center text-slate-600 dark:text-slate-400 font-semibold p-1.5 rounded-full group-hover:bg-indigo-200 group-hover:text-indigo-600">
-          <Icon name="mdi:share-outline" class="w-5 h-5" />
-        </button>
-        <p
-          class="text-slate-600 dark:text-slate-400 font-semibold tracking-wide group-hover:text-indigo-600"
-        >
-          102
-        </p>
-      </div>
-    </div>
-    <div class="">
-      <TheComment @refresh="refresh()" v-for="comment in post.comments" :key="comment.id" :comment="comment" />
+      </ClientOnly>
     </div>
     <UModal v-model="open" :ui="{
       container: 'flex min-h-full items-end items-center justify-center text-center'
@@ -83,7 +99,8 @@
 </template>
 
 <script lang="ts" setup>
-
+import { Vue3Lottie } from 'vue3-lottie';
+import astro from '~/public/astro.json';
 import type { Post, Like, Comment } from '~/types/post';
 
 const { id } = useRoute().params
@@ -101,11 +118,11 @@ const config = useRuntimeConfig()
 const open = ref(false)
 
 useHead({
-  title: `ASTROOO - ${post.value.users.username}'s posts`,
+  title: `ASTROOO - ${post.value?.users?.username}'s posts`,
   meta: [
     {
       name: "description",
-      content: post.value.content.substring(0, 100)
+      content: post.value?.content?.substring(0, 100)
     }
   ]
 
